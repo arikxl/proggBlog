9f16d261bcf002972559b3cbf2da439c138613b0
Arik Alexandrov <arikxl@gmail.com>
2023-03-03 00:30:36 +0200

    global style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,36 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+
+import {  PostCard, Categories, PostWidget} from '../components';
+
+const posts = [
+  { id:101, title: 'בדיקת ריאקט ' , excerpt: 'אחלה דבר שבעולם! ' },
+  {id: 102, title: 'Ts Test', excerpt: 'Ts Test 11111  OMG' }
+]
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="container mx-auto px-10 mb-8 ">
       <Head>
-        <title>Create Next App</title>
+        <title>ProggBlog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-        ARIK
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+
+        <div className='lg: col-span-8 col-span-1' >
+          {posts.map(post => (
+            <PostCard post={ post} key={ post.id }/>
+          ))}
+        </div>
+
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
