3700ec72041c7038e410148b58f1b2f422915854
Arik Alexandrov <arikxl@gmail.com>
2023-03-06 01:08:47 +0200

    posts cards working & fetch posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,11 @@
+import Head from 'next/head';
 import type { NextPage } from 'next'
-import Head from 'next/head'
 
+import { getPosts } from '../services';
 import {  PostCard, Categories, PostWidget} from '../components';
 
-const posts = [
-  { id:101, title: 'בדיקת ריאקט ' , excerpt: 'אחלה דבר שבעולם! ' },
-  {id: 102, title: 'Ts Test', excerpt: 'Ts Test 11111  OMG' }
-]
 
-const Home: NextPage = () => {
+const Home: NextPage = ({posts}) => {
   return (
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
@@ -18,8 +15,8 @@ const Home: NextPage = () => {
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
 
         <div className='lg: col-span-8 col-span-1' >
-          {posts.map(post => (
-            <PostCard post={ post} key={ post.id }/>
+          {posts.map((post) => (
+            <PostCard key={post.id} post={ post.node} />
           ))}
         </div>
 
@@ -35,3 +32,10 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: {posts}
+  }
+}
