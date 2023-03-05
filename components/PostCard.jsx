b713172f3ea3f5c63d6ca842555f5b587d53bc94
Arik Alexandrov <arikxl@gmail.com>
2023-03-05 23:55:46 +0200

    post card & services

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
-const PostCard = ({post}) => {
+
+
+const PostCard = ({ post }) => {
+  console.log('post:', post)
   return (
-      <div>
-          {post.title}
-          {post.excerpt}
+    <div className='bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8'>
+      <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
+        <img src={post.featuredImage} alt={post.title}
+          className='object-top absolute h-80 w-full object-cover shadow-lg
+        rounded-t-lg lg:rounded-lg'/>
+      </div>
+      {post.title}
+      {post.excerpt}
     </div>
   )
 }
