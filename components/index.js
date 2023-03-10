99df524f540b74bce1215857cb7f1867d3a76eb2
Arik Alexandrov <arikxl@gmail.com>
2023-03-10 23:36:50 +0200

    more comps

--- components/index.js
+++ components/index.js
@@ -1,6 +1,10 @@
 export { default as Layout } from './Layout';
+export { default as Author } from './Author';
+export { default as Comments } from './Comments';
 export { default as PostCard } from './PostCard';
 export { default as AppHeader } from './AppHeader';
 export { default as AppFooter } from './AppFooter';
 export { default as Categories } from './Categories';
 export { default as PostWidget } from './PostWidget';
+export { default as PostDetail } from './PostDetail';
+export { default as CommentsForm } from './CommentsForm';
